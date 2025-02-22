5e4a988cbd52aafe95b0d7e180337c36683f76fe
o-tao <dhtkdals369@gmail.com>
2025-02-22 15:17:44 +0900

    Lv.0 문자열 바꿔서 찾기

---
+++ src/programmers/year_2025_February/day_22/Tao_문자열_바꿔서_찾기.java
@@ -0,0 +1,21 @@
+package programmers.year_2025_February.day_22;
+
+public class Tao_문자열_바꿔서_찾기 {
+
+    public static void main(String[] args) {
+        Tao_문자열_바꿔서_찾기 answer = new Tao_문자열_바꿔서_찾기();
+
+        String myString = "ABBAA";
+        String pat = "AABB";
+
+        System.out.println(answer.solution(myString, pat));
+    }
+
+    public int solution(String myString, String pat) {
+        myString = myString.replace("A", "X")
+                .replace("B", "A")
+                .replace("X", "B");
+
+        return myString.contains(pat) ? 1 : 0;
+    }
+}
