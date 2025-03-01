6144e4111bebe6c2898864dbfddfec3e8be814c8
o-tao <dhtkdals369@gmail.com>
2025-03-01 22:48:12 +0900

    Lv.0 문자열 묶기

---
+++ src/programmers/year_2025/march/day_01/Tao_문자열_묶기.java
@@ -0,0 +1,27 @@
+package programmers.year_2025.march.day_01;
+
+public class Tao_문자열_묶기 {
+
+    public static void main(String[] args) {
+        Tao_문자열_묶기 tao = new Tao_문자열_묶기();
+
+        String[] strArr = {"a", "bc", "d", "efg", "hi"};
+
+        System.out.println(tao.solution(strArr));
+    }
+
+    public int solution(String[] strArr) {
+        int answer = 0;
+        int[] countArr = new int[30];
+        for (String str : strArr) {
+            countArr[str.length() - 1]++;
+        }
+
+        for (int max : countArr) {
+            if (answer < max) {
+                answer = max;
+            }
+        }
+        return answer;
+    }
+}
