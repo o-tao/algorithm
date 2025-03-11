8a2fb3f9f6a82607a7b7a44d314ff24691a517fb
o-tao <dhtkdals369@gmail.com>
2025-03-11 21:29:58 +0900

    Lv.0 커피 심부름

---
+++ src/programmers/year_2025/march/day_11/Tao_커피_심부름.java
@@ -0,0 +1,18 @@
+package programmers.year_2025.march.day_11;
+
+import java.util.Arrays;
+
+public class Tao_커피_심부름 {
+
+    public static void main(String[] args) {
+        Tao_커피_심부름 tao = new Tao_커피_심부름();
+
+        String[] order = {"cafelatte", "americanoice", "hotcafelatte", "anything"};
+
+        System.out.println(tao.solution(order));
+    }
+
+    public int solution(String[] order) {
+        return Arrays.stream(order).mapToInt(item -> item.contains("cafelatte") ? 5000 : 4500).sum();
+    }
+}
