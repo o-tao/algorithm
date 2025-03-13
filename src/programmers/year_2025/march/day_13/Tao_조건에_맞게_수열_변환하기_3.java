0bc335705ce43b81554143459ee2efc3f6f48d69
o-tao <dhtkdals369@gmail.com>
2025-03-13 18:17:43 +0900

    Lv.0 조건에 맞게 수열 변환하기

---
+++ src/programmers/year_2025/march/day_13/Tao_조건에_맞게_수열_변환하기_3.java
@@ -0,0 +1,19 @@
+package programmers.year_2025.march.day_13;
+
+import java.util.Arrays;
+
+public class Tao_조건에_맞게_수열_변환하기_3 {
+
+    public static void main(String[] args) {
+        Tao_조건에_맞게_수열_변환하기_3 tao = new Tao_조건에_맞게_수열_변환하기_3();
+
+        int[] arr = {1, 2, 3, 100, 99, 98};
+        int k = 3;
+
+        System.out.println(Arrays.toString(tao.solution(arr, k)));
+    }
+
+    public int[] solution(int[] arr, int k) {
+        return k % 2 == 0 ? Arrays.stream(arr).map(i -> i + k).toArray() : Arrays.stream(arr).map(i -> i * k).toArray();
+    }
+}
