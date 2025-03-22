4581fb1560a5d738e0dbee8805f5e466ea068bd0
o-tao <dhtkdals369@gmail.com>
2025-03-23 00:12:11 +0900

    Lv.1 나머지가 1이 되는 수 찾기

---
+++ src/programmers/year_2025/march/day_23/Tao_나머지가_1이_되는_수_찾기.java
@@ -0,0 +1,18 @@
+package programmers.year_2025.march.day_23;
+
+import java.util.stream.IntStream;
+
+public class Tao_나머지가_1이_되는_수_찾기 {
+
+    public static void main(String[] args) {
+        Tao_나머지가_1이_되는_수_찾기 tao = new Tao_나머지가_1이_되는_수_찾기();
+
+        int n = 10;
+
+        System.out.println(tao.solution(n));
+    }
+
+    public int solution(int n) {
+        return IntStream.rangeClosed(1, n).filter(i -> n % i == 1).min().orElse(0);
+    }
+}
