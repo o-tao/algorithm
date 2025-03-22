22f385a9082c610bd2818abe6ff343883c6b7f76
o-tao <dhtkdals369@gmail.com>
2025-03-22 21:32:38 +0900

    Lv.1 x만큼 간격이 있는 n개의 숫자

---
+++ src/programmers/year_2025/march/day_22/Tao_x만큼_간격이_있는_n개의_숫자.java
@@ -0,0 +1,19 @@
+package programmers.year_2025.march.day_22;
+
+import java.util.Arrays;
+import java.util.stream.LongStream;
+
+public class Tao_x만큼_간격이_있는_n개의_숫자 {
+
+    public static void main(String[] args) {
+        Tao_x만큼_간격이_있는_n개의_숫자 tao = new Tao_x만큼_간격이_있는_n개의_숫자();
+
+        int x = 2, n = 5;
+
+        System.out.println(Arrays.toString(tao.solution(x, n)));
+    }
+
+    public long[] solution(int x, int n) {
+        return LongStream.rangeClosed(1, n).map(i -> x * i).toArray();
+    }
+}
