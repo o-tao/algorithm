b26020b97835b6ccf035c9e18f25db2b0691f633
o-tao <dhtkdals369@gmail.com>
2025-03-25 22:02:22 +0900

    Lv.1 정수 내림차순으로 배치하기

---
+++ src/programmers/year_2025/march/day_25/Tao_정수_내림차순으로_배치하기.java
@@ -0,0 +1,21 @@
+package programmers.year_2025.march.day_25;
+
+import java.util.Arrays;
+
+public class Tao_정수_내림차순으로_배치하기 {
+
+    public static void main(String[] args) {
+        Tao_정수_내림차순으로_배치하기 tao = new Tao_정수_내림차순으로_배치하기();
+
+        long n = 118372;
+
+        System.out.println(tao.solution(n));
+    }
+
+    public long solution(long n) {
+        char[] answer = String.valueOf(n).toCharArray();
+        Arrays.sort(answer);
+        StringBuilder sb = new StringBuilder(new String(answer));
+        return Long.parseLong(sb.reverse().toString());
+    }
+}
