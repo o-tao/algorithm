6a5f2b04202ba75a7022c2c7e6b6eb68da5b706f
o-tao <dhtkdals369@gmail.com>
2025-04-04 21:16:03 +0900

    Lv.1 제일 작은 수 제거하기

---
+++ src/programmers/year_2025/april/Day_04_제일_작은_수_제거하기.java
@@ -0,0 +1,20 @@
+package programmers.year_2025.april;
+
+import java.util.Arrays;
+
+public class Day_04_제일_작은_수_제거하기 {
+
+    public static void main(String[] args) {
+        Day_04_제일_작은_수_제거하기 tao = new Day_04_제일_작은_수_제거하기();
+
+        int[] arr = {4, 3, 2, 1};
+
+        System.out.println(Arrays.toString(tao.solution(arr)));
+    }
+
+    public int[] solution(int[] arr) {
+        int min = Arrays.stream(arr).min().orElse(0);
+        int[] answer = Arrays.stream(arr).filter(i -> i != min).toArray();
+        return answer.length == 0 ? new int[]{-1} : answer;
+    }
+}
