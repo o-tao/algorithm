f9b1c039f7ff6c0cec6329481ce4ea8d524a191b
o-tao <dhtkdals369@gmail.com>
2025-04-09 21:46:56 +0900

    Lv.1 부족한 금액 계산하기

---
+++ src/programmers/year_2025/april/Day_09_부족한_금액_계산하기.java
@@ -0,0 +1,21 @@
+package programmers.year_2025.april;
+
+import java.util.stream.LongStream;
+
+public class Day_09_부족한_금액_계산하기 {
+
+    public static void main(String[] args) {
+        Day_09_부족한_금액_계산하기 tao = new Day_09_부족한_금액_계산하기();
+
+        int price = 3;
+        int money = 20;
+        int count = 4;
+
+        System.out.println(tao.solution(price, money, count));
+    }
+
+    public long solution(int price, int money, int count) {
+        long totalPrice = LongStream.rangeClosed(0, count).map(i -> price * i).sum();
+        return totalPrice < money ? 0 : totalPrice - money;
+    }
+}
