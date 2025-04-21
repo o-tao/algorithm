60b617083711e379b9e21e85e32ed86ab11fe3d8
o-tao <dhtkdals369@gmail.com>
2025-04-21 21:39:15 +0900

    Lv.1 가장 가까운 같은 글자

---
+++ src/programmers/year_2025/april/Day_21_가장_가까운_같은_글자.java
@@ -0,0 +1,29 @@
+package programmers.year_2025.april;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.IntStream;
+
+public class Day_21_가장_가까운_같은_글자 {
+
+    public static void main(String[] args) {
+        Day_21_가장_가까운_같은_글자 tao = new Day_21_가장_가까운_같은_글자();
+
+        String s = "banana";
+
+        System.out.println(Arrays.toString(tao.solution(s)));
+    }
+
+    public int[] solution(String s) {
+        Map<Character, Integer> lastIndexMap = new HashMap<>();
+
+        return IntStream.range(0, s.length())
+                .map(i -> {
+                    char c = s.charAt(i);
+                    int result = lastIndexMap.containsKey(c) ? i - lastIndexMap.get(c) : -1;
+                    lastIndexMap.put(c, i);
+                    return result;
+                }).toArray();
+    }
+}
