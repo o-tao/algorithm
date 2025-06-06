041adabab3d635eca7dba8b57909e39accc82c66
o-tao <dhtkdals369@gmail.com>
2025-06-06 23:33:34 +0900

    Lv.1 완주하지 못한 선수

---
+++ src/programmers/year_2025/july/Day_06_완주하지_못한_선수.java
@@ -0,0 +1,36 @@
+package programmers.year_2025.july;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Day_06_완주하지_못한_선수 {
+
+    public static void main(String[] args) {
+        Day_06_완주하지_못한_선수 tao = new Day_06_완주하지_못한_선수();
+
+        String[] participant = {"leo", "kiki", "eden"};
+        String[] completion = {"eden", "kiki"};
+
+        System.out.println(tao.solution(participant, completion));
+    }
+
+    public String solution(String[] participant, String[] completion) {
+        Map<String, Integer> map = new HashMap<>();
+
+        for (String name : participant) {
+            map.put(name, map.getOrDefault(name, 0) + 1);
+        }
+
+        for (String name : completion) {
+            map.put(name, map.get(name) - 1);
+        }
+
+        for (String key : map.keySet()) {
+            if (map.get(key) != 0) {
+                return key;
+            }
+        }
+
+        return "";
+    }
+}
