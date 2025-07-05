21c8e767ef8fa93ec702a5cb06564291c9bdbfb9
o-tao <dhtkdals369@gmail.com>
2025-07-05 15:32:21 +0900

    Lv.2 괄호 회전하기

--- src/programmers_java/year_2025/July/Day_05_괄호_회전하기.java
+++ src/programmers_java/year_2025/July/Day_05_괄호_회전하기.java
@@ -1,5 +1,44 @@
 package programmers_java.year_2025.July;
 
+import java.util.Stack;
+
 public class Day_05_괄호_회전하기 {
-    // 7월 5일
+    public static void main(String[] args) {
+        Day_05_괄호_회전하기 tao = new Day_05_괄호_회전하기();
+
+        String s = "[](){}";
+
+        System.out.println(tao.solution(s));
+    }
+
+    public int solution(String s) {
+        int answer = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            String rotated = s.substring(i) + s.substring(0, i);
+            if (isValid(rotated)) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+
+    private boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) return false;
+                char top = stack.pop();
+                if ((c == ')' && top != '(') ||
+                        (c == ']' && top != '[') ||
+                        (c == '}' && top != '{')) {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
 }
