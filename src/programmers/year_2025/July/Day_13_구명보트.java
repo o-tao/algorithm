640aa7cc12fb857ef14241055887cabc87258e91
o-tao <dhtkdals369@gmail.com>
2025-07-13 23:33:50 +0900

    Lv.2 구명보트

---
+++ src/programmers/year_2025/July/Day_13_구명보트.java
@@ -0,0 +1,33 @@
+package programmers.year_2025.July;
+
+import java.util.Arrays;
+
+public class Day_13_구명보트 {
+
+    public static void main(String[] args) {
+        Day_13_구명보트 tao = new Day_13_구명보트();
+
+        int[] people = {70, 50, 80, 50};
+        int limit = 100;
+
+        System.out.println(tao.solution(people, limit));
+    }
+
+    public int solution(int[] people, int limit) {
+        Arrays.sort(people);
+        int left = 0;
+        int right = people.length - 1;
+        int answer = 0;
+
+        while (left <= right) {
+            if (people[left] + people[right] <= limit) {
+                left++;
+            }
+
+            right--;
+            answer++;
+        }
+
+        return answer;
+    }
+}
