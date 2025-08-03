cb248d1771bfc65f492c934a98a833b9014b3c87
o-tao <dhtkdals369@gmail.com>
2025-08-03 20:36:29 +0900

    Lv.2 배열 자르기

--- src/programmers_java/year_2025/august/Day_03_배열_자르기.java
+++ src/programmers_java/year_2025/august/Day_03_배열_자르기.java
@@ -1,5 +1,27 @@
 package programmers_java.year_2025.august;
 
+import java.util.Arrays;
+
 public class Day_03_배열_자르기 {
-    // 8월 3일
+    public static void main(String[] args) {
+        Day_03_배열_자르기 tao = new Day_03_배열_자르기();
+
+        int n = 3;
+        long left = 2;
+        long right = 5;
+
+        System.out.println(Arrays.toString(tao.solution(n, left, right)));
+    }
+
+    public int[] solution(int n, long left, long right) {
+        int length = (int) (right - left + 1);
+        int[] answer = new int[length];
+
+        for (long i = left; i <= right; i++) {
+            long row = i / n;
+            long col = i % n;
+            answer[(int) (i - left)] = (int) (Math.max(row, col) + 1);
+        }
+        return answer;
+    }
 }
