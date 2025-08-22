89838ff795066f3519318bca08f6046c39377bd2
o-tao <dhtkdals369@gmail.com>
2025-08-22 20:21:53 +0900

    Lv.2 행렬의 곱셈

--- src/programmers_java/year_2025/august/Day_22_행렬의_곱셈.java
+++ src/programmers_java/year_2025/august/Day_22_행렬의_곱셈.java
@@ -1,5 +1,33 @@
 package programmers_java.year_2025.august;
 
+import java.util.Arrays;
+
 public class Day_22_행렬의_곱셈 {
-    // 8월 22일
+    public static void main(String[] args) {
+        Day_22_행렬의_곱셈 tao = new Day_22_행렬의_곱셈();
+
+        int[][] arr1 = {{1, 4}, {3, 2}, {4, 1}};
+        int[][] arr2 = {{3, 3}, {3, 3}};
+
+        System.out.println(Arrays.deepToString(tao.solution(arr1, arr2)));
+    }
+
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int m = arr1.length;
+        int n = arr1[0].length;
+        int p = arr2[0].length;
+
+        int[][] answer = new int[m][p];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < p; j++) {
+                int sum = 0;
+                for (int k = 0; k < n; k++) {
+                    sum += arr1[i][k] * arr2[k][j];
+                }
+                answer[i][j] = sum;
+            }
+        }
+        return answer;
+    }
 }
