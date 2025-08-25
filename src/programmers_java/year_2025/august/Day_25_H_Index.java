bf395e72891a2caf480385b59d996b471153ddb8
o-tao <dhtkdals369@gmail.com>
2025-08-25 19:11:23 +0900

    Lv.2 H-Index

--- src/programmers_java/year_2025/august/Day_25_H_Index.java
+++ src/programmers_java/year_2025/august/Day_25_H_Index.java
@@ -1,5 +1,29 @@
 package programmers_java.year_2025.august;
 
+import java.util.Arrays;
+
 public class Day_25_H_Index {
-    // 8월 25일
+    public static void main(String[] args) {
+        Day_25_H_Index tao = new Day_25_H_Index();
+
+        int[] citations = {3, 0, 6, 1, 5};
+
+        System.out.println(tao.solution(citations));
+    }
+
+    public int solution(int[] citations) {
+        Arrays.sort(citations);
+        int n = citations.length;
+        int answer = 0;
+
+        for (int i = 0; i < n; i++) {
+            int hIndex = n - i;
+            if (citations[i] >= hIndex) {
+                answer = hIndex;
+                break;
+            }
+        }
+
+        return answer;
+    }
 }
