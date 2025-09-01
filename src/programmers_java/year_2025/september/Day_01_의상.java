2b40b41f5c2c5aa1027ef424836dab3147de8b68
o-tao <dhtkdals369@gmail.com>
2025-09-01 21:43:03 +0900

    Lv.2 의상

--- src/programmers_java/year_2025/september/Day_01_의상.java
+++ src/programmers_java/year_2025/september/Day_01_의상.java
@@ -1,4 +1,30 @@
 package programmers_java.year_2025.september;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Day_01_의상 {
+    public static void main(String[] args) {
+        Day_01_의상 tao = new Day_01_의상();
+
+        String[][] clothes = {{"yellow_hat", "headgear"}, {"blue_sunglasses", "eyewear"}, {"green_turban", "headgear"}};
+
+        System.out.println(tao.solution(clothes));
+    }
+
+    public int solution(String[][] clothes) {
+        Map<String, Integer> map = new HashMap<>();
+
+        for (String[] cloth : clothes) {
+            String type = cloth[1];
+            map.put(type, map.getOrDefault(type, 0) + 1);
+        }
+
+        int answer = 1;
+        for (int count : map.values()) {
+            answer *= (count + 1);
+        }
+
+        return answer - 1;
+    }
 }
