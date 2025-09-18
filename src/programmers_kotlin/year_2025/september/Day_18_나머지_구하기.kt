aeb9a29e948d968cc95b6766dc78bc323ba8099b
o-tao <dhtkdals369@gmail.com>
2025-09-18 23:52:22 +0900

    Lv.0 kotlin: 나머지 구하기

---
+++ src/programmers_kotlin/year_2025/september/Day_18_나머지_구하기.kt
@@ -0,0 +1,9 @@
+package programmers_kotlin.year_2025.september
+
+private class Day_18_나머지_구하기 {
+    fun solution(num1: Int, num2: Int): Int = num1 % num2
+}
+
+fun main() {
+    println(Day_18_나머지_구하기().solution(3, 2))
+}
