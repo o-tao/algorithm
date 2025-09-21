c1d5481af1e047bac0f6a01040a095253c3f6439
o-tao <dhtkdals369@gmail.com>
2025-09-21 22:36:46 +0900

    Lv.0 kotlin: 두 수의 차 구하기

---
+++ src/programmers_kotlin/year_2025/september/Day_21_두_수의_차_구하기.kt
@@ -0,0 +1,9 @@
+package programmers_kotlin.year_2025.september
+
+private class Day_21_두_수의_차_구하기 {
+    fun solution(num1: Int, num2: Int): Int = num1 - num2
+}
+
+fun main() {
+    println(Day_21_두_수의_차_구하기().solution(2, 3))
+}
