bd7d553354fc27996d3116c8b52c0204337d746a
o-tao <dhtkdals369@gmail.com>
2025-09-23 22:43:41 +0900

    Lv.0 kotlin: 나이 출력

---
+++ src/programmers_kotlin/year_2025/september/Day_23_나이_출력.kt
@@ -0,0 +1,9 @@
+package programmers_kotlin.year_2025.september
+
+private class Day_23_나이_출력 {
+    fun solution(age: Int): Int = 2022 - age + 1
+}
+
+fun main() {
+    println(Day_23_나이_출력().solution(40))
+}
