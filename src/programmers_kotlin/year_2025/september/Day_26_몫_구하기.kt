29facde08ec79d01ce470d1df5e88709c81b5e2f
o-tao <dhtkdals369@gmail.com>
2025-09-26 15:05:02 +0900

    Lv.0 kotlin: 몫 구하기

---
+++ src/programmers_kotlin/year_2025/september/Day_26_몫_구하기.kt
@@ -0,0 +1,9 @@
+package programmers_kotlin.year_2025.september
+
+private class Day_26_몫_구하기 {
+    fun solution(num1: Int, num2: Int) = num1 / num2
+}
+
+fun main() {
+    println(Day_26_몫_구하기().solution(10, 5))
+}
