2b5abf8ca4008392b90474bfc55de7164d0a72d3
o-tao <dhtkdals369@gmail.com>
2025-09-27 22:15:46 +0900

    Lv.0 kotlin: 두 수의 곱 구하기

---
+++ src/programmers_kotlin/year_2025/september/Day_27_두_수의_곱_구하기.kt
@@ -0,0 +1,9 @@
+package programmers_kotlin.year_2025.september
+
+private class Day_27_두_수의_곱_구하기 {
+    fun solution(num1: Int, num2: Int) = num1 * num2
+}
+
+fun main() {
+    println(Day_27_두_수의_곱_구하기().solution(3, 4))
+}
