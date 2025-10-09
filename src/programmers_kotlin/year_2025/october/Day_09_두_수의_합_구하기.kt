c2f5d3bbdbd21a617b1633295a579166c413dca5
o-tao <dhtkdals369@gmail.com>
2025-10-09 15:57:02 +0900

    Lv.0 kotlin: 두 수의 합 구하기

---
+++ src/programmers_kotlin/year_2025/october/Day_09_두_수의_합_구하기.kt
@@ -0,0 +1,9 @@
+package programmers_kotlin.year_2025.october
+
+class Day_09_두_수의_합_구하기 {
+    fun solution(num1: Int, num2: Int) = num1 + num2
+}
+
+fun main() {
+    println(Day_09_두_수의_합_구하기().solution(2, 3))
+}
