268408f80e70421d611c2e5fd773acfe7cdacbc4
o-tao <dhtkdals369@gmail.com>
2025-10-18 17:38:36 +0900

    Lv.0 kotlin: 배열의 평균값

---
+++ src/programmers_kotlin/year_2025/october/Day_18_배열의_평균값.kt
@@ -0,0 +1,9 @@
+package programmers_kotlin.year_2025.october
+
+private class Day_18_배열의_평균값 {
+    fun solution(numbers: IntArray) = numbers.average()
+}
+
+fun main() {
+    println(Day_18_배열의_평균값().solution(intArrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)))
+}
