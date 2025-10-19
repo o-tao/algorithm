0ee29de63b903f286171780fcb5de0f30e4383e8
o-tao <dhtkdals369@gmail.com>
2025-10-19 17:15:09 +0900

    Lv.0 kotlin: 배열 뒤집기

---
+++ src/programmers_kotlin/year_2025/october/Day_19_배열_뒤집기.kt
@@ -0,0 +1,9 @@
+package programmers_kotlin.year_2025.october
+
+private class Day_19_배열_뒤집기 {
+    fun solution(num_list: IntArray) = num_list.reversed()
+}
+
+fun main() {
+    println(Day_19_배열_뒤집기().solution(intArrayOf(1, 2, 3, 4, 5)))
+}
