f831a63552d8738d7ce74eeb9b62846c6d6d4a46
o-tao <dhtkdals369@gmail.com>
2025-10-20 13:07:57 +0900

    Lv.0 kotlin: 뒤집힌 문자열

---
+++ src/programmers_kotlin/year_2025/october/Day_20_뒤집힌_문자열.kt
@@ -0,0 +1,9 @@
+package programmers_kotlin.year_2025.october
+
+private class Day_20_뒤집힌_문자열 {
+    fun solution(my_string: String) = my_string.reversed()
+}
+
+fun main() {
+    println(Day_20_뒤집힌_문자열().solution("jaron"))
+}
