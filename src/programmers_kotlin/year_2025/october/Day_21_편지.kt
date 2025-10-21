f5522744c482a8c7f1dc87fb96b8379220e716be
o-tao <dhtkdals369@gmail.com>
2025-10-21 16:52:30 +0900

    Lv.0 kotlin: 편지

---
+++ src/programmers_kotlin/year_2025/october/Day_21_편지.kt
@@ -0,0 +1,9 @@
+package programmers_kotlin.year_2025.october
+
+private class Day_21_편지 {
+    fun solution(message: String) = message.length * 2
+}
+
+fun main() {
+    println(Day_21_편지().solution("happy birthday!"))
+}
