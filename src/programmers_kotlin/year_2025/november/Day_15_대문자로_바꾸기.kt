1d8f2cf9f1e3fbc578c62aa61324169d27d54ea1
o-tao <dhtkdals369@gmail.com>
2025-11-15 21:50:03 +0900

    Lv.0 kotlin: 대문자로 바꾸기

---
+++ src/programmers_kotlin/year_2025/november/Day_15_대문자로_바꾸기.kt
@@ -0,0 +1,9 @@
+package programmers_kotlin.year_2025.november
+
+private class Day_15_대문자로_바꾸기 {
+    fun solution(myString: String) = myString.uppercase()
+}
+
+fun main() {
+    println(Day_15_대문자로_바꾸기().solution("aBcDeFg"))
+}
