d26baba0a80c1dc8458919a618b9b97bcf37590f
o-tao <dhtkdals369@gmail.com>
2025-11-20 17:14:44 +0900

    Lv.0 kotlin: 문자 리스트를 문자열로 변환하기

---
+++ src/programmers_kotlin/year_2025/november/Day_20_문자_리스트를_문자열로_변환하기.kt
@@ -0,0 +1,9 @@
+package programmers_kotlin.year_2025.november
+
+private class Day_20_문자_리스트를_문자열로_변환하기 {
+    fun solution(arr: Array<String>) = arr.joinToString("")
+}
+
+fun main() {
+    println(Day_20_문자_리스트를_문자열로_변환하기().solution(arrayOf("a", "b", "c")))
+}
