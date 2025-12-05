b7bfd7d892c204eb89db8a51fba42242a8b6b539
o-tao <dhtkdals369@gmail.com>
2025-12-05 16:56:43 +0900

    Lv.0 kotlin: 카운트 업

---
+++ src/programmers_kotlin/year_2025/december/Day_05_카운트_업.kt
@@ -0,0 +1,9 @@
+package programmers_kotlin.year_2025.december
+
+private class Day_05_카운트_업 {
+    fun solution(start_num: Int, end_num: Int) = (start_num..end_num).toList()
+}
+
+fun main() {
+    println(Day_05_카운트_업().solution(3, 10))
+}
