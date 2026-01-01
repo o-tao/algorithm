d62cab46e19a7207a0ea13d1de2572d236292c35
o-tao <dhtkdals369@gmail.com>
2026-01-01 22:14:27 +0900

    Lv.0 kotlin: 정수 부분

---
+++ src/programmers_kotlin/year_2026/january/Day_01_정수_부분.kt
@@ -0,0 +1,9 @@
+package programmers_kotlin.year_2026.january
+
+private class Day_01_정수_부분 {
+    fun solution(flo: Double) = flo.toInt()
+}
+
+fun main() {
+    println(Day_01_정수_부분().solution(1.42))
+}
