0f2234c55e9377e435125d02dc66635cbd01f9e3
o-tao <dhtkdals369@gmail.com>
2026-01-05 20:09:27 +0900

    Lv.0 kotlin: 정수 찾기

--- src/programmers_kotlin/year_2026/january/Day_05_정수_찾기.kt
+++ src/programmers_kotlin/year_2026/january/Day_05_정수_찾기.kt
@@ -1,4 +1,9 @@
 package programmers_kotlin.year_2026.january
 
-class Day_05_정수_찾기 {
+private class Day_05_정수_찾기 {
+    fun solution(num_list: IntArray, n: Int) = if (num_list.contains(n)) 1 else 0
+}
+
+fun main() {
+    println(Day_05_정수_찾기().solution(intArrayOf(1, 2, 3, 4, 5), 3))
 }
