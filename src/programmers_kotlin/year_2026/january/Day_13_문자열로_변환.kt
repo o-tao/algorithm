0ebaefc4251abcb5567f7860ebf1ddaacff33a54
o-tao <dhtkdals369@gmail.com>
2026-01-13 16:13:25 +0900

    Lv.0 kotlin: 문자열로 변환

---
+++ src/programmers_kotlin/year_2026/january/Day_13_문자열로_변환.kt
@@ -0,0 +1,9 @@
+package programmers_kotlin.year_2026.january
+
+private class Day_13_문자열로_변환 {
+    fun solution(n: Int) = n.toString()
+}
+
+fun main() {
+    println(Day_13_문자열로_변환().solution(123))
+}
