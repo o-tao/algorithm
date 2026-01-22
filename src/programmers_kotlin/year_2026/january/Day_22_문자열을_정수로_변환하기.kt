47042e142e4a68d357c72f08418b25a154ae1bf6
o-tao <dhtkdals369@gmail.com>
2026-01-22 22:08:06 +0900

    Lv.0 kotlin: 문자열을 정수로 변환하기

---
+++ src/programmers_kotlin/year_2026/january/Day_22_문자열을_정수로_변환하기.kt
@@ -0,0 +1,9 @@
+package programmers_kotlin.year_2026.january
+
+private class Day_22_문자열을_정수로_변환하기 {
+    fun solution(n_str: String) = n_str.toInt()
+}
+
+fun main() {
+    println(Day_22_문자열을_정수로_변환하기().solution("10"))
+}
