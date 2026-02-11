422cd86658cd40c605ad36e3601c2f4704d31f8f
o-tao <dhtkdals369@gmail.com>
2026-02-11 23:30:11 +0900

    Lv.0 kotlin: 소문자로 바꾸기

---
+++ src/programmers_kotlin/year_2026/february/Day_11_소문자로_바꾸기.kt
@@ -0,0 +1,9 @@
+package programmers_kotlin.year_2026.february
+
+private class Day_11_소문자로_바꾸기 {
+    fun solution(my_string: String) = my_string.lowercase()
+}
+
+fun main() {
+    println(Day_11_소문자로_바꾸기().solution("aBcDeFg"))
+}
