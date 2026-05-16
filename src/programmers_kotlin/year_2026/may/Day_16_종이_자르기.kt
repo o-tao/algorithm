ad3aceec6115aa0879e5fdd560546d6034724e00
o-tao <dhtkdals369@gmail.com>
2026-05-16 23:37:51 +0900

    Lv.0 kotlin: 종이 자르기

---
+++ src/programmers_kotlin/year_2026/may/Day_16_종이_자르기.kt
@@ -0,0 +1,9 @@
+package programmers_kotlin.year_2026.may
+
+private class Day_16_종이_자르기 {
+    fun solution(M: Int, N: Int) = (M * N) - 1
+}
+
+fun main() {
+    println(Day_16_종이_자르기().solution(2, 2))
+}
