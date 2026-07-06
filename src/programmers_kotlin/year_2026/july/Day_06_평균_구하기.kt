05d59c080e3a99fcdd8e47ecae2f81b9db3e6b29
o-tao <dhtkdals369@gmail.com>
2026-07-06 22:47:42 +0900

    Lv.1 kotlin: 평균 구하기

---
+++ src/programmers_kotlin/year_2026/july/Day_06_평균_구하기.kt
@@ -0,0 +1,9 @@
+package programmers_kotlin.year_2026.july
+
+private class Day_06_평균_구하기 {
+    fun solution(arr: IntArray) = arr.average()
+}
+
+fun main() {
+    println(Day_06_평균_구하기().solution(intArrayOf(1, 2, 3, 4)))
+}
